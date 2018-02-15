56a5a51c65a67abdd515cb8d8c3d26dcf7a9a75f
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-02-15 15:59:53 -0500

    Parse input and check stackability
    Currently able to parse the input and check if it is stackable. Stacking the boxes function is still in progress.

---
+++ stacking.py
@@ -0,0 +1,76 @@
+import pdb
+
+def is_stackable(total_size, num_stacks):
+    """Checks if the boxes are capable of being stacked evenly
+
+        total_size (int): The sum of all the boxes size added together
+        num_stacks (int): The number of stacks to make out of the boxes
+
+    Returns:
+        bool: True if stackable, False otherwise.
+    """
+    if total_size % num_stacks == 0:
+
+        return True
+
+    else:
+
+        return False
+
+
+def parse(stack_info):
+    """Parses the string to find stack size and number of stacks to make
+
+        stack_info (str): String containing stack size and # stacks to make
+
+    Returns:
+        tuple: Tuple returning total size, number of stacks, and box sizes
+    """
+
+    total_size = 0
+    parsed = stack_info.split(" ")
+
+    for number in parsed[1]:
+        total_size += int(number)
+
+    num_stacks = int(parsed[0])
+    boxes = parsed[1]
+    return (total_size, num_stacks, boxes)
+
+def stack_boxes(total_size, num_stacks, boxes):
+    """Stacks boxes evenly based on the requirements passed in
+
+        total_size (int): The sum of all the boxes size added together
+        num_stacks (int): The number of stacks to make out of the boxes
+        boxes      (str): list of all the sizes of boxes
+
+    Returns:
+        None (TBD?)
+    """
+
+    group_size = int(total_size / num_stacks)
+    sum = 0
+    diff = group_size
+    stack = ""
+
+
+# String containing the the amount of stacks and all the size of each box
+string = "3 34312332"
+
+total_size, num_stacks, boxes = parse(string)
+
+print("\nstack #: {}".format(num_stacks))
+print("total stack: {}".format(total_size))
+
+# Check if we can make x amount of stacks evenly
+if is_stackable(total_size, num_stacks):
+
+    print("\nIt's stackable")
+    stack_boxes(total_size, num_stacks, boxes)
+
+else:
+
+    print("\nnot stackable")
+
+print("\n")
+
